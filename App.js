863b3aa12e5aa2d8f59b28c9add65efbb0277028
Pedro-Clear <pedro.xavier.clear@gmail.com>
2023-08-21 17:51:12 -0300

    Creating Page Home

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 
@@ -5,11 +6,15 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes/index';
 
+import AuthProvider from './src/contexts/auth';
+
 export default function App(){
   return(
     <NavigationContainer>
-      <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content" />
-      <Routes/>
+      <AuthProvider>
+        <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content" />
+        <Routes/>
+      </AuthProvider>
     </NavigationContainer>
   )
 }
