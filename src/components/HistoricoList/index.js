8dae51d8c78414c67c11939277ddb110e3550620
Pedro-Clear <pedro.xavier.clear@gmail.com>
2023-08-23 01:15:46 -0300

    List Gastos 2

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {Container, TipoText} from './styles';
+import {
+  Container, 
+  TipoText,
+  Tipo,
+  IconView,
+  ValorText
+} from './styles';
 
-export default function HistoricoList(){
+import Icon from 'react-native-vector-icons/Feather'
+
+export default function HistoricoList({ data }){
   return(
     <Container>
-      <TipoText> TESTE TIPO </TipoText>
+      
+      <Tipo>
+        <IconView tipo={data.type}>
+          <Icon 
+            name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'}
+            size={20} 
+            color="#FFF" 
+          />
+          <TipoText>{data.type}</TipoText>
+        </IconView>
+      </Tipo>
+
+      <ValorText>
+        R$ {data.value}
+      </ValorText>
+
     </Container>
   )
 }
