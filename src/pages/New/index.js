0837f2f8c43ff3b4569af3b874dbaf5ec9892141
Pedro-Clear <pedro.xavier.clear@gmail.com>
2023-08-22 21:45:52 -0300

    Component type register

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Background, Input, SubmitButton, SubmitText } from './styles';
 import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Header from '../../components/Header'
+import RegisterTypes from '../../components/RegisterTypes'
 
 export default function New(){
+  const [labelInput, setLabelInput] = useState('');
+  const [valueInput, setValueInput] = useState('');
+  const [type, setType] = useState('receita');
+
   return(
     <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() } >
       <Background>
@@ -14,12 +19,18 @@ export default function New(){
         <SafeAreaView style={{marginTop: 14, alignItems: 'center' }}>
           <Input
             placeholder="Descrição desse registro"
+            value={labelInput}
+            onChangeText={ (text) => setLabelInput(text) }
           />
 
           <Input
             placeholder="Valor desejado"
             keyboardType="numeric"
+            value={valueInput}
+            onChangeText={ (text) => setValueInput(text) }
           />
+
+          <RegisterTypes type={type} sendTypeChanged={ (item) => setType(item) } />
 
           <SubmitButton>
             <SubmitText>Registrar</SubmitText>
